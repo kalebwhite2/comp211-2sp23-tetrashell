fefdf1df199b1efb29d8fc28e2f5556e4af5366f
Kaleb William White <kalebw02@comp211-2sp23.cs.unc.edu>
2023-04-25 10:31:18 -0400

    first commit

--- tetrashell.c
+++ tetrashell.c
@@ -1,2 +1,40 @@
 #include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
 
+#define STDLINESIZE 128
+
+char* read_line();
+
+int main() {
+    char* arg;
+    while(strcmp(arg = read_line(), "exit")) {
+        printf("%s\n", arg);
+    }
+    free(arg);
+}
+
+char* read_line() {
+    int line_size = STDLINESIZE;
+    char* buffer = malloc(sizeof(char) * line_size);
+    int idx = 0;
+    int ch;
+
+    if (!buffer) {
+        fprintf(stderr, "allocation error in read_line");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("tetrashell> ");
+    while (1) {
+        ch = getchar();
+
+        if (ch == EOF || ch == '\n') {
+            buffer[idx] =  '\0';
+            return buffer;
+        } else {
+            buffer[idx] = ch;
+            idx++;
+        }
+    }
+}
