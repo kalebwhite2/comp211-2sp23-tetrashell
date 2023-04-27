40156a33dc9bd292dd07285f18636d70efaeb2ac
Kaleb William White <kalebw02@comp211-2sp23.cs.unc.edu>
2023-04-27 11:27:27 -0400

    added intro_art, welcome functionO

--- tetrashell.c
+++ tetrashell.c
@@ -5,43 +5,60 @@
 #define STDLINESIZE 128
 
 char* read_line();
-char** parse_args(char* args){
-    char** buffed = malloc(sizeof(char*));
-    int size = 1;
-    char* created;
-    int i;
-    i = 0;
-    created = strtok(args, " ");
-    while (created != NULL) {
-	    if (i >= size) {
-              size *= 2;
-	      buffed = realloc(buffed, size*sizeof(char*));
-	    }
-	    buffed[i] = strdup(created);
-	    printf("%s\n", created);
-	    created = strtok(NULL, " ");
-	    i++;
-    }
-    if (i >= size) {
-       buffed = realloc(buffed, (size + 1)*sizeof(char*));
-    }
-    buffed[i] = NULL;
-    return buffed;
-}
+char** parse_args(char* args);
+char* welcome();
+void print_image(FILE *fptr);
 
 int main() {
     char* arg;
     char** args; 
-    while(strcmp(arg = read_line(), "exit")) {
-        //printf("%s\n", arg);
+
+    char* pathname = welcome();
+
+    //init loop 
+    printf("tetrashell> ");
+    arg = read_line();
+    while(strcmp(arg, "exit")) {
+
         args = parse_args(arg);
         if (!strcmp(args[0], "recover")) {
            printf("found recover\n");
-	   break; 
+	       break; 
         }
-	free(args);
+	    free(args);
+
+        //run next
+        printf("tetrashell> ");
+        arg = read_line();
     }
+
     free(arg);
+}
+
+char* welcome() {
+    //print out the intro text from intro_art.txt
+    FILE * imgpath = fopen("intro_art.txt", "r");
+    if (imgpath == NULL) {
+        fprintf(stderr, "unable to open image \n");
+        exit(EXIT_FAILURE);    
+    }
+    print_image(imgpath);
+    fclose(imgpath);
+
+    printf("WELCOME TO TETRASHELL\nTHE SHELL OF YOUR DREAMS\nENTER THE PATH TO YOUR QUICKSAVE: \n");
+
+    char* pathname = read_line(); 
+    
+    return pathname;
+}
+
+#define MAXIMGLINESIZE 128
+void print_image(FILE * imgpath) {
+    char str[MAXIMGLINESIZE];
+
+    while(fgets(str, sizeof(str), imgpath) != NULL) {
+        printf("%s", str);
+    }
 }
 
 char* read_line() {
@@ -55,7 +72,6 @@ char* read_line() {
         exit(EXIT_FAILURE);
     }
 
-    printf("tetrashell> ");
     while (1) {
         ch = getchar();
 
@@ -68,6 +84,27 @@ char* read_line() {
         }
     }
     return buffer;
+}
+
+char** parse_args(char* args){
+    char** buffed = malloc(sizeof(char*));
+    int size = 1, i = 0;
+    char* created;
+    created = strtok(args, " ");
+    while (created != NULL) {
+	    if (i >= size) {
+              size *= 2;
+	      buffed = realloc(buffed, size*sizeof(char*));
+	    }
+	    buffed[i] = strdup(created);
+	    created = strtok(NULL, " ");
+	    i++;
+    }
+    if (i >= size) {
+       buffed = realloc(buffed, (size + 1)*sizeof(char*));
+    }
+    buffed[i] = NULL;
+    return buffed;
 }
 
 
